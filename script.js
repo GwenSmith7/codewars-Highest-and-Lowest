79787e48c6d92b796eff21fdbceac704fdf8c011
gs86 <44513004+GwenSmith7@users.noreply.github.com>
2022-03-11 09:11:09 -0500

    Second Commit *with* the solution
    Added solution

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+function highAndLow(numbers) {
+  //   console.log(numbers)
+
+  // turn string to array so we can sort
+  const numArr = numbers.split(' ').map(Number);
+
+  // sort in ascending order
+  numArr.sort((a, b) => a - b);
+
+  // create an empty array so we can push the last and first element into it
+  const result = [];
+
+  // push last element into new array
+  result.push(numArr[numArr.length - 1]);
+
+  // push first element into array
+  result.push(numArr[0]);
+
+  // .join outputs a string with the specified seperator (space)
+  return result.join(' ');
+
+  console.log(numArr);
+
+  // sort numbers
+
+  // This code needs to be refactored
+}
